baeceaf625b5dd11caf5ff1c1c77e257312505ec
KombinatorVit <slonik0775@gmail.com>
2023-03-17 23:03:14 +0200

    header and nav

---
+++ src/skills/Skills.js
@@ -0,0 +1,11 @@
+import styles from './Skills.module.css'
+
+const Skills = () => {
+    return (
+        <div className={styles.skillsBlock}>
+
+        </div>
+    );
+};
+
+export default Skills;
